74acbbc5574ede5804b452cba2cba3923f0d80df
Ertugrul Dogruluk <id5540@alunos.uminho.pt>
2020-07-07 17:51:01 +0100

    Update ndntube.cpp
    revision

--- scenarios/ndntube.cpp
+++ scenarios/ndntube.cpp
@@ -1,3 +1,23 @@
+/**  This program is free software: you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation, either version 3 of the License, or
+  (at your option) any later version.
+
+ This program is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.
+ You should have received a copy of the GNU General Public License
+ along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+Copyright (c) 2019, Algorithmi Research Centree, University of Minho, Portugal
+
+
+ * Authors: Ertugrul Dogruluk, Antonio Costa. <d7474@di.uminho.pt>, Department of #Informatics
+
+**/
+
+
 #include "../extensions/BadGuy.h"
 #include "../extensions/content-store-privacy.hpp"
 
